096c04bb3e2df39e9e09e08322ce6dd47db975c9
Neeraj Dalal <admin@nrjdalal.com>
2023-11-01 11:03:40 +0530

    support for bash added

--- bin/script.sh
+++ bin/script.sh
@@ -2,14 +2,14 @@
 mkdir -p ~/.pglaunch
 
 if [[ "$SHELL" == "/bin/zsh" ]]; then
-  curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.zsh | cat >~/.pglaunch/fx.zsh
+  curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.sh | cat >~/.pglaunch/fx.zsh
   grep "source ~/.pglaunch/fx.zsh" ~/.zshrc &>/dev/null || echo "source ~/.pglaunch/fx.zsh" >>~/.zshrc
 
   exec zsh
 fi
 
 if [[ "$SHELL" == "/bin/bash" ]]; then
-  curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.bash | cat >~/.pglaunch/fx.bash
+  curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.sh | cat >~/.pglaunch/fx.bash
   grep "source ~/.pglaunch/fx.bash" ~/.bashrc &>/dev/null || echo "source ~/.pglaunch/fx.bash" >>~/.bashrc
 
   exec bash
