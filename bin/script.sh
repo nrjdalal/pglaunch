bc3b416fc94dd315eff039fdfa86587b24328c7e
Neeraj Dalal <admin@nrjdalal.com>
2025-03-11 18:09:32 +0530

    chore: tweaks to script.sh package.json ~ 2 files 12+ 12-
    bin/script.sh
    package.json

--- bin/script.sh
+++ bin/script.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 mkdir -p ~/.pglaunch
 
-if command -v pglaunch &>/dev/null; then
-  echo "pglaunch is already installed!"
-  echo "Please run 'pglaunch -h' to get started."
-  return 0
-fi
-
 if [[ "$SHELL" == "/bin/zsh" ]]; then
   curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.sh | cat >~/.pglaunch/fx.zsh
   grep "source ~/.pglaunch/fx.zsh" ~/.zshrc &>/dev/null || echo "source ~/.pglaunch/fx.zsh" >>~/.zshrc
 
-  echo "pglaunch successfully installed for zsh!"
-  echo "Please run 'pglaunch -h' to get started."
+  echo
+  echo "pglaunch successfully installed!"
+  echo -e "\e[36mPlease run 'pglaunch -h' to get started.\e[0m"
+  echo
   exec zsh
   return 0
 fi
@@ -21,13 +17,17 @@ if [[ "$SHELL" == "/bin/bash" ]]; then
   curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.sh | cat >~/.pglaunch/fx.bash
   grep "source ~/.pglaunch/fx.bash" ~/.bashrc &>/dev/null || echo "source ~/.pglaunch/fx.bash" >>~/.bashrc
 
-  echo "pglaunch successfully installed for bash!"
+  echo
+  echo "pglaunch successfully installed!"
   echo "Please run 'pglaunch -h' to get started."
+  echo
   exec bash
   return 0
 fi
 
-echo "Unsupported shell: $SHELL"
-echo "Please use bash or zsh, addtionally you can manually source the fx.sh file in this repo."
+echo
+echo "Unsupported shell: $SHELL!"
+echo "Please use bash or zsh, addtionally you can manually source the fx.sh file from the repository."
 echo "More shells to be supported soon."
+echo
 return 1
