a711dd5725541a5b17d84928f65b002d521d12df
Neeraj Dalal <admin@nrjdalal.com>
2025-03-11 18:07:11 +0530

    chore: tweaks to script.sh package.json ~ 2 files 16+ 4-
    bin/script.sh
    package.json

--- bin/script.sh
+++ bin/script.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 mkdir -p ~/.pglaunch
 
+if command -v pglaunch &>/dev/null; then
+  echo "pglaunch is already installed!"
+  echo "Please run 'pglaunch -h' to get started."
+  return 0
+fi
+
 if [[ "$SHELL" == "/bin/zsh" ]]; then
   curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.sh | cat >~/.pglaunch/fx.zsh
   grep "source ~/.pglaunch/fx.zsh" ~/.zshrc &>/dev/null || echo "source ~/.pglaunch/fx.zsh" >>~/.zshrc
 
-  echo "Please restart your shell to use pglaunch (e.g. exec zsh)"
+  echo "pglaunch successfully installed for zsh!"
+  echo "Please run 'pglaunch -h' to get started."
+  exec zsh
+  return 0
 fi
 
 if [[ "$SHELL" == "/bin/bash" ]]; then
   curl -s https://raw.githubusercontent.com/nrjdalal/pglaunch/master/bin/fx.sh | cat >~/.pglaunch/fx.bash
   grep "source ~/.pglaunch/fx.bash" ~/.bashrc &>/dev/null || echo "source ~/.pglaunch/fx.bash" >>~/.bashrc
 
-  echo "Please restart your shell to use pglaunch (e.g. exec bash)"
+  echo "pglaunch successfully installed for bash!"
+  echo "Please run 'pglaunch -h' to get started."
+  exec bash
+  return 0
 fi
 
 echo "Unsupported shell: $SHELL"
 echo "Please use bash or zsh, addtionally you can manually source the fx.sh file in this repo."
 echo "More shells to be supported soon."
-exit 1
+return 1
